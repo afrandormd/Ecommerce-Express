dc591bafdab475f045a445ccadc3d403e508a991
afrandormd <afrandormd@gmail.com>
2025-09-20 23:05:17 +0700

    refactor: disable swagger-ui

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import { readFile } from "fs/promises";
+// import swaggerUi from "swagger-ui-express";
+// import { readFile } from "fs/promises";
 
 const app = express();
 
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const raw = await readFile("./docs/apidoc.json", "utf-8");
-const swaggerDocument = JSON.parse(raw);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// const raw = await readFile("./docs/apidoc.json", "utf-8");
+// const swaggerDocument = JSON.parse(raw);
+//
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("API is Running!");
