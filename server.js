6313e01c21a9653db4e99ba1ae0baed7f19896a6
afrandormd <afrandormd@gmail.com>
2025-09-20 23:36:23 +0700

    fix: fixing error

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./src/routes/user.routes.js";
-// import swaggerUi from "swagger-ui-express";
-// import { readFile } from "fs/promises";
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// const raw = await readFile("./docs/apidoc.json", "utf-8");
-// const swaggerDocument = JSON.parse(raw);
-//
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("API is Running!");
@@ -24,6 +15,4 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 
-app.listen(PORT, () => {
-  `Server started on port ${port}`;
-});
+export default app;
