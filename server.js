867a22947a4cf51fa25b632687c489e0af0f387f
afrandormd <afrandormd@gmail.com>
2025-09-20 23:02:22 +0700

    chore: setup swagger-ui

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { readFile } from "fs/promises";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const raw = await readFile("./docs/apidoc.json", "utf-8");
+const swaggerDocument = JSON.parse(raw);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("API is Running!");
