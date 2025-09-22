5619675987c6a079a7191da1ef8f85b190665397
afrandormd <afrandormd@gmail.com>
2025-09-22 19:41:56 +0700

    feat(auth): create service login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { errorResponse, successResponse } from "../utils/responses.js";
 import { createUser } from "../services/user.service.js";
+import cookieOptions from "../utils/cookieOptions.js";
 
 // Register
 export const register = async (req, res) => {
@@ -23,5 +24,32 @@ export const register = async (req, res) => {
     id: user.id,
     name: user.name,
     email: user.email,
+  });
+};
+
+// Login
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find User
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return errorResponse(res, "Email Not Registered", null, 404);
+
+  // Matching Password
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword)
+    return errorResponse(res, "Password Invalid", null, 401);
+
+  // Creating JWT Token
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, cookieOptions(req));
+
+  return successResponse(res, "Login Successfully!", {
+    userId: user.id,
+    email: email,
+    token: token,
   });
 };
